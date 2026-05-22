6d2489b359352f2be1e903baef8d20ad5be27f27
Cristina Manganotti <cristina.eventos.sandra@gmail.com>
2026-05-22 14:16:33 -0300

    first commit

---
+++ conversor_temperatura.py
@@ -0,0 +1,7 @@
+celsius_input = input("Digite a temperatura em graus Celsius: ")
+celsius = float(celsius_input)
+
+# Aplica a fórmula matemática para converter Celsius em Fahrenheit
+fahrenheit = (celsius * 9 / 5) + 32
+
+print(fahrenheit)
